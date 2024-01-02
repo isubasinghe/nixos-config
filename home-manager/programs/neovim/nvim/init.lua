be189103e3de25760a1a9f3eb28db833b8a45b2e
isubasinghe <isubasinghe@student.unimelb.edu.au>
2024-01-02 13:03:05 +1100

    added vim-illuminate

--- home-manager/programs/neovim/nvim/init.lua
+++ home-manager/programs/neovim/nvim/init.lua
@@ -105,7 +105,8 @@ require("lazy").setup({
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.4',
       dependencies = { 'nvim-lua/plenary.nvim' }
-  }
+  },
+  { 'RRethy/vim-illuminate' }
 })
 
 

4db0152dc14119534fdc8754d971065e5e3b5f5e
isubasinghe <i.subasinghe@unsw.edu.au>
2024-01-10 12:26:37 +1100

    set z3 version

--- nixos/lab-config.nix
+++ nixos/lab-config.nix
@@ -127,7 +127,7 @@
     enable = true;
     settings = {
       PermitRootLogin = "no";
-      PasswordAuthentication = false;
+      PasswordAuthentication = true;
       GatewayPorts = "yes";
     };
   };

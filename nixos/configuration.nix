7947c9f542054ae98ef32ed00c8885e5fe71105d
isubasinghe <i.subasinghe@unsw.edu.au>
2024-04-14 13:56:25 +1000

    auto update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -211,6 +211,18 @@
     127.0.0.1 mysql
     127.0.0.1 azurite
   '';
+
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L" # print build logs
+    ];
+    dates = "02:00";
+    randomizedDelaySec = "45min";
+  };
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
 }
